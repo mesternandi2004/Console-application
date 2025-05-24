85023cf686d513ea7c516ae214a6a54e6d6d23f2
mesternandor04@gmail.com <mesternandor04@gmail.com>
2025-05-24 10:52:01 +0200

    StorageService - (Events for storages + Storage logic)

--- CQW1QQ_HSZF_2024251.Application/StorageService.cs
+++ CQW1QQ_HSZF_2024251.Application/StorageService.cs
@@ -6,5 +6,71 @@ using CQW1QQ_HSZF_2024251.Persistence.MsSql.Repository.Interfaces;
 
 namespace CQW1QQ_HSZF_2024251.Application
 {
-    
+    public class StorageService : IStorageService
+    {
+        public event EventHandler<string>? CriticalCapacityDetected;
+
+        private readonly IFridgeRepository _fridgeRepository;
+        private readonly IPantryRepository _pantryRepository;
+
+        public StorageService(IFridgeRepository fridgeRepository, IPantryRepository pantryRepository)
+        {
+            _fridgeRepository = fridgeRepository;
+            _pantryRepository = pantryRepository;
+        }
+
+        public bool PutAwayProduct(ref Product product)
+        {
+            if (product.StoreInFridge)
+            {
+                var fridge = _fridgeRepository.ReadAll().FirstOrDefault();
+                double remainingCapacity = GetRemainingCapacity(fridge);
+                CheckCriticalCapacity("Fridge", remainingCapacity, fridge.Capacity);
+
+                if (remainingCapacity - product.Quantity >= 0)
+                {
+                    fridge.ProductIds.Add(product.Id);
+                    fridge.Products.Add(product);
+
+                    product.Storage = fridge;
+                    return true;
+                }
+            }
+            else
+            {
+                var pantry = _pantryRepository.ReadAll().FirstOrDefault();
+                double remainingCapacity = GetRemainingCapacity(pantry);
+                CheckCriticalCapacity("Pantry", remainingCapacity, pantry.Capacity);
+
+                if (remainingCapacity - product.Quantity >= 0)
+                {
+                    pantry.ProductIds.Add(product.Id);
+                    pantry.Products.Add(product);
+                    product.Storage = pantry;
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool CheckCriticalCapacity(string storageName, double remainingCapacity, double maxCapacity)
+        {
+            if (remainingCapacity / maxCapacity < 0.1)
+            {
+                OnCriticalCapacityDetected(storageName);
+                return true;
+            }
+            return false;
+        }
+        public double GetRemainingCapacity(Storage storage)
+        {
+            double totalUsedCapacity = storage.Products.Sum(p => p.Quantity);
+            return storage.Capacity - totalUsedCapacity;
+        }
+        protected virtual void OnCriticalCapacityDetected(string storageName)
+        {
+            CriticalCapacityDetected?.Invoke(this, storageName);
+        }
+    }
 }
