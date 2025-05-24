babd825c9591ae4a0de404519fabee907c24d1c1
mesternandor04@gmail.com <mesternandor04@gmail.com>
2025-05-24 10:52:56 +0200

    Services Interfaces Implement

--- CQW1QQ_HSZF_2024251.Application/Interfaces/IProductService.cs
+++ CQW1QQ_HSZF_2024251.Application/Interfaces/IProductService.cs
@@ -2,5 +2,14 @@
 
 namespace CQW1QQ_HSZF_2024251.Application.Interfaces
 {
-    
+    public interface IProductService
+    {
+        event EventHandler<Product>? CriticalStockDetected;
+        event EventHandler<(Person, Product)>? FavoriteProductRestocked;
+
+        void ConsumeProduct(int productId, double quantity);
+        IEnumerable<Product> GetCriticalStockProducts();
+        IEnumerable<Product> GetNearExpiryProducts(int daysToExpire);
+        void RestockProduct(int productId, int quantity);
+    }
 }
--- CQW1QQ_HSZF_2024251.Application/Interfaces/IStorageService.cs
+++ CQW1QQ_HSZF_2024251.Application/Interfaces/IStorageService.cs
@@ -2,5 +2,13 @@
 
 namespace CQW1QQ_HSZF_2024251.Application.Interfaces
 {
-  
+    public interface IStorageService
+    {
+        event EventHandler<string>? CriticalCapacityDetected;
+
+        double GetRemainingCapacity(Storage storage);
+        bool PutAwayProduct(ref Product product);
+
+        bool CheckCriticalCapacity(string storageName, double remainingCapacity, double maxCapacity);
+    }
 }
